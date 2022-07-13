26b5e016be4ab2d8537f166bf5c6a1c7a513b264
Alexander Riccio <test35965@gmail.com>
2022-07-13 17:26:00 -0400

    only output if something might be available

--- run.rb
+++ run.rb
@@ -5,15 +5,18 @@ require 'gems'
 
 # RUBYGEMS_API_BASE_URL = 'https://rubygems.org/api/v2/rubygems/'
 
+CURRENT_VERSION = '6.0.0.beta2'
 # def check_validates_timeliness
 #     validates_timeliness_in_rubygems = "#{RUBYGEMS_API_BASE_URL}/validates_timeliness"
 # end
 
 def run
+
     correct_latest_version = Gems.versions('validates_timeliness')[0]["number"]
-    
-    pp "latest correct version: #{correct_latest_version}"
-    pp "\"latest_version\" #{Gems.latest_version('validates_timeliness')}"
+    if (correct_latest_version != CURRENT_VERSION)
+        pp "Possible new version available: #{correct_latest_version}"
+    end
+    # pp "\"latest_version\" #{Gems.latest_version('validates_timeliness')}"
 end
 
 run
